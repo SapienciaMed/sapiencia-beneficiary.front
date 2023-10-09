27352ad78c896e940044f7ec2d41fe0c524d49d1
Daniel Flores <dflores@i4digital.com>
2023-10-09 17:59:46 -0500

    docume

---
+++ src/_app.tsx
@@ -0,0 +1,50 @@
+import React, { Suspense, lazy, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppContextProvider } from "./common/contexts/app.context";
+import "./styles/_app.scss";
+import "./styles/_theme-prime.css";
+import "primereact/resources/primereact.min.css";
+import ModalMessageComponent from "./common/components/modal-message.component";
+import ApplicationProvider from "./application-provider";
+import useAppCominicator from "./common/hooks/app-communicator.hook";
+import DocumentsRoutes from "./features/documents/documents-routes";
+import PortalAuthRoutes from "./features/portal-auth/portal-auth.routes";
+
+function App() {
+  const { publish } = useAppCominicator();
+  const HomePage = lazy(() => import("./common/components/home.page"));
+
+  // Effect que comunica la aplicacion actual
+  useEffect(() => {
+    localStorage.setItem("currentAplication", process.env.aplicationId);
+    setTimeout(
+      () => publish("currentAplication", process.env.aplicationId),
+      500
+    );
+  }, []);
+
+  return (
+    <AppContextProvider>
+      <ModalMessageComponent />
+      <ApplicationProvider>
+        <Router>
+          <Suspense fallback={<p>Loading...</p>}>
+            <Routes>
+              <Route path={"/beneficiario/"} element={<HomePage />} />;
+              <Route
+                path={"/beneficiario/documentos/*"}
+                element={<DocumentsRoutes />}
+              />
+               <Route
+                path={"/portal/*"}
+                element={<PortalAuthRoutes />}
+              />
+            </Routes>
+          </Suspense>
+        </Router>
+      </ApplicationProvider>
+    </AppContextProvider>
+  );
+}
+
+export default React.memo(App);
